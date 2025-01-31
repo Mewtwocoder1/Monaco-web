008fad992610fda4d3afacb666c17182f350e1f2
Mewtwocoder <130798336+Mewtwocoder1@users.noreply.github.com>
2025-01-31 14:14:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,17 +6,15 @@ require(["vs/editor/editor.main"], function () {
   const disposeEmmet = emmetMonaco.emmetHTML(monaco, ['html', 'php']);
 
   // Now create the Monaco editor instance
-  const editor = monaco.editor.create(document.getElementById('container'), {
+  window.editor = monaco.editor.create(document.getElementById('container'), {
     language: 'html',
     theme: 'vs-dark',
   });
-  
-  document.getElementById("preview");
-  editor.addEventListener("keydown", function (e) {
-    let text = editor.getValue();
-    preview.contentDocument.body.innerHTML = text;
-  });
-  
-  });
+});
+
+document.getElementById("preview");
+window.editor.addEventListener("keydown", function (e) {
+  let value = window.editor.getValue();
+  preview.contentDocument.body.innerHTML = (value);
 });
 
